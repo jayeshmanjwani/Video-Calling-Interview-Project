c76c1d9d7349ee0b6871d75f879730f1460d4cce
jayeshmanjwani <jayeshmanjwani@gmail.com>
2026-03-22 19:40:32 +0530

    Initial Commit

---
+++ backend/src/lib/env.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+export const ENV = {
+    PORT: process.env.PORT,
+    DB_URL: process.env.DB_URL,
+};
