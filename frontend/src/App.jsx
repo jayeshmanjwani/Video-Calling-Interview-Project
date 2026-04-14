b27e58354d57327312cc216246146c5de0f094e4
jayeshmanjwani <jayeshmanjwani@gmail.com>
2026-04-15 00:02:41 +0530

    landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,15 @@ import HomePage from "./Pages/HomePage";
 import ProblemsPage from "./Pages/ProblemsPage";
 
 function App() {
-  const {isSignedIn} = useUser();
+  const {isSignedIn,isLoaded} = useUser();
+
+  // This will get rid of flickering effect 
+  if(!isLoaded) return null; 
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard" />} />
+      <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />} />
       <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />} /> 
     </Routes>
   )
