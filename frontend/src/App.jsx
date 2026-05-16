db7d2a6b2f9c109094a4e25536e745460877ede4
jayeshmanjwani <jayeshmanjwani@gmail.com>
2026-05-16 23:49:04 +0530

    Typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
+import HomePage from "./Pages/HomePage";
 
 import { Toaster } from "react-hot-toast";
-import DashboardPage from "./pages/DashboardPage";
-import ProblemPage from "./pages/ProblemPage";
-import ProblemsPage from "./pages/ProblemsPage";
-import SessionPage from "./pages/SessionPage";
+import DashboardPage from "./Pages/DashboardPage";
+import ProblemPage from "./Pages/ProblemPage";
+import ProblemsPage from "./Pages/ProblemsPage";
+import SessionPage from "./Pages/SessionPage";
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
