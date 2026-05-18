018593389dd2ae091ecf383785433b9638d07319
jayeshmanjwani <jayeshmanjwani@gmail.com>
2026-05-18 23:42:57 +0530

    minor changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const rawApiUrl = import.meta.env.VITE_API_URL || "";
+const apiBaseUrl = rawApiUrl.replace(/\/api\/?$/, "");
+
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${apiBaseUrl}/api`,
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
