09cae4c4357b7ebbc8a495d221bb524ab8701b6f
jayeshmanjwani <jayeshmanjwani@gmail.com>
2026-06-05 20:22:38 +0530

    minor changes in aiRoutes for suspicious paste count

--- backend/src/routes/aiRoutes.js
+++ backend/src/routes/aiRoutes.js
@@ -1,13 +1,32 @@
 import express from "express";
+import Session from "../models/Session.js";
 
 const router = express.Router();
 
-router.post("/tab-switch", async (req, res) => {
-   res.json({ success: true });
-});
-
 router.post("/paste-detection", async (req, res) => {
-   res.json({ success: true });
+  try {
+    const { sessionId } = req.body;
+
+    await Session.findByIdAndUpdate(
+      sessionId,
+      {
+        $inc: {
+          suspiciousPasteCount: 1,
+        },
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+    });
+  }
 });
 
 export default router;
